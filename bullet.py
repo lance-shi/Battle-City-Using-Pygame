626cef0f3f85951f95809caca20c984aa68e3dae
Lance <Lanceyshi@gmail.com>
2021-03-30 23:49:23 +1100

    add bullet shooting mechanism

---
+++ bullet.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Bullet:
+	(DIR_UP, DIR_RIGHT, DIR_DOWN, DIR_LEFT) = range(4)
+
+	def __init__(self, screen, sprites, x, y, direction, speed=5):
+		self.screen = screen
+		self.x = x
+		self.y = y
+		self.direction = direction
+		self.speed = speed
+		self.image = sprites.subsurface(75*2, 74*2, 3*2, 4*2)
+		position = [self.x, self.y]
+
+		if self.direction == self.DIR_UP:
+			self.rect = pygame.Rect(position[0] + 11, position[1] - 8, 6, 8)
+		elif self.direction == self.DIR_RIGHT:
+			self.image = pygame.transform.rotate(self.image, 270)
+			self.rect = pygame.Rect(position[0] + 26, position[1] + 11, 8, 6)
+		elif self.direction == self.DIR_DOWN:
+			self.image = pygame.transform.rotate(self.image, 180)
+			self.rect = pygame.Rect(position[0] + 11, position[1] + 26, 6, 8)
+		elif self.direction == self.DIR_LEFT:
+			self.image = pygame.transform.rotate(self.image, 90)
+			self.rect = pygame.Rect(position[0] - 8 , position[1] + 11, 8, 6)
+
+	def update(self):
+		if self.direction == self.DIR_UP:
+			self.rect.y -= self.speed
+		if self.direction == self.DIR_LEFT:
+			self.rect.x -= self.speed
+		if self.direction == self.DIR_RIGHT:
+			self.rect.x += self.speed
+		if self.direction == self.DIR_DOWN:
+			self.rect.y += self.speed
+
+	def draw(self):
+		self.screen.blit(self.image, self.rect.topleft)
--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
+from bullet import *
 
 class Game:
+	(DIR_UP, DIR_RIGHT, DIR_DOWN, DIR_LEFT) = range(4)
+
 	def __init__(self):
 		size = self.width, self.height = 480, 416
 
@@ -11,31 +14,42 @@ class Game:
 		
 		self.player = self.sprites.subsurface(0, 0, 13*2, 13*2)
 		pygame.display.set_icon(self.player)
+		self.bullets = []
 		self.mainLoop()
 
 	def mainLoop(self):
 		run = True
 		playerRect = pygame.Rect(100, 300, 13*2, 13*2)
+		tankSpeed = 2
 		while run:
 			self.clock.tick(60)
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					run = False
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_SPACE:
+						newBullet = Bullet(self.screen, self.sprites, playerRect.x, playerRect.y, self.DIR_UP)
+						self.bullets.append(newBullet)
+
+			for bullet in self.bullets:
+				bullet.update()
 
 			keys_pressed = pygame.key.get_pressed()
-			if keys_pressed[pygame.K_a] and playerRect.x - 5 > 0:
-				playerRect.x -= 5
-			if keys_pressed[pygame.K_s] and playerRect.y + playerRect.height + 5 < self.height:
-				playerRect.y += 5
-			if keys_pressed[pygame.K_d] and playerRect.x + playerRect.width + 5 < self.width:
-				playerRect.x += 5
-			if keys_pressed[pygame.K_w] and playerRect.y - 5 > 0:
-				playerRect.y -= 5
+			if keys_pressed[pygame.K_a] and playerRect.x - tankSpeed > 0:
+				playerRect.x -= tankSpeed
+			if keys_pressed[pygame.K_s] and playerRect.y + playerRect.height + tankSpeed < self.height:
+				playerRect.y += tankSpeed
+			if keys_pressed[pygame.K_d] and playerRect.x + playerRect.width + tankSpeed < self.width:
+				playerRect.x += tankSpeed
+			if keys_pressed[pygame.K_w] and playerRect.y - tankSpeed > 0:
+				playerRect.y -= tankSpeed
 			self.draw(playerRect)
-
+				
 		pygame.quit()
 
 	def draw(self, playerRect):
 		self.screen.fill([0, 0, 0])
-		self.screen.blit(self.player, (playerRect.x, playerRect.y))
+		self.screen.blit(self.player, playerRect.topleft)
+		for bullet in self.bullets:
+			bullet.draw()
 		pygame.display.update()
